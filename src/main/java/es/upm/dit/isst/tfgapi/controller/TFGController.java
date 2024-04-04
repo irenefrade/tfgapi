7853b0b530f80f95dfec6254ce4ac8b4a2619b61
irene <irene.frade@alumnos.upm.es>
2024-04-04 20:50:59 +0200

    version inicial

--- src/main/java/es/upm/dit/isst/tfgapi/controller/TFGController.java
+++ src/main/java/es/upm/dit/isst/tfgapi/controller/TFGController.java
@@ -11,21 +11,9 @@ import org.hibernate.validator.internal.util.logging.LoggerFactory;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-import org.slf4j.*;
 
-
-import ch.qos.logback.classic.Logger;
 import es.upm.dit.isst.tfgapi.model.Estado;
 import es.upm.dit.isst.tfgapi.model.Sesion;
 import es.upm.dit.isst.tfgapi.model.TFG;
